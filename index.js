d0ad00dab24cdc4ae8c1f65a5430148d441934f1
Anthony Mwaniki <anthonymwangi072@gmail.com>
2022-08-05 08:44:02 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,44 @@
+function add() {
+    return 1+80;
+}
+function subtract() {
+    return 60-40;
+}
+function multiply() {
+    return 2*3.4;
+}
+function divide() {
+    return 5.0/2.5;
+}
+function add(a, b) {  
+    return a + b;  
+}  
+function subtract(a, b) {  
+    return a - b;  
+}  
+function multiply(a, b) {  
+    return a * b;  
+} 
+function divide(a, b) {  
+    return a / b;  
+}   
+function increment(n) {  
+    n++;
+    return(n)  
+}   
+function decrement(n) {  
+    n--;
+    return(n)  
+}   
+function makeInt(n){
+    return (n);
+}
+function makeInt(n){
+    return parseInt(n, 10);
+}
+// function makeInt(n){
+//     return parseFloat(n);
+// }
+function preserveDecimal(n) {
+     return parseFloat(n)
+}
